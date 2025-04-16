b9f68891b28479edcc220e8e7308bb1ac26a60db
prathapchitra <107908670+prathapchitra@users.noreply.github.com>
2025-04-16 17:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Hello from Jenkins CI/CD test and production!')
+  response.send('Hello World! Hello from Jenkins CI/CD test and production for synycs!')
 })
 
 app.listen(app.get('port'), function() {
