7d0e11acecb2103d7b77ec2f72fa4eb81ce63eaa
prathapchitra <107908670+prathapchitra@users.noreply.github.com>
2025-04-28 17:27:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Stanford capital is leading outsourcing firm based in Mumbai and UAE. We are solution providers in the areas of Finance and Accounts, compliance expertise, Information Technology, Human Resources and Business Process Outsourcing. We strive to provide innovative and practical solutions that can be easily implemented. As a solution provider, we empathize with our clients and prioritize understanding their business and tackling their challenges as if they were our own. !')
+  response.send('welcome to synycs!')
 })
 
 app.listen(app.get('port'), function() {
